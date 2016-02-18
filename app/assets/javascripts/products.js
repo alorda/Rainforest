f7e22a456ba6bcf32bb0bed183eae677eff5f12c
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-02-18 16:50:53 -0500

    Finish infinite scroll on Rainforest

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,17 +1,27 @@
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
-//
 
 $(document).on('ready page:load', function() {
-
   $('#search-form').submit(function(event) {
     event.preventDefault();
     var searchValue = $('#search').val();
 
-    $.getScript('products?search=' + searchValue);
+    $.ajax({
+     url: '/products?search=' + $('#search').val(),
+     type: 'GET',
+     dataType: 'script'
     });
   });
+
+    // $.getScript('products?search=' + searchValue);
+
+    $(window).scroll(function() {
+      var url = $('.pagination span.next').children().attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Fetching more products...");
+        return $.getScript(url);
+      }
+   });
+});
+
 
 
   // <script type="text/javascript">
