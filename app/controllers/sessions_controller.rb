a4984d2d04ef4600586fb5ee05797315b190bbc6
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-02-17 22:01:08 -0500

    Create product partial, make sure products only show once (they were repeating), implement AJAX search and XHR object.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-  skip_before_action :ensure_logged_in, except: [:destroy]
 
   def new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@
     resources :reviews, only: [:show, :create, :destroy]
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 end
 
